00f6cdb7027b4787b9125ac1fc72f01c3ddb4d62
jesanchezcr <jesanchezcr@unal.edu.co>
2015-03-26 09:59:45 -0500

    MergeSortJava report

--- src/MergeSortJava.java
+++ src/MergeSortJava.java
@@ -6,52 +6,62 @@ public class MergeSortJava extends SortMethod{
     MergeSortJava(String sortName, long instanceSize) {
         super(sortName, instanceSize);
     }
-
-    @Override
-    public int[] sortM(int[] array)
-    {
-        if(array.length > 1)
-        {
+    int mov;
+    int comp;
+    public int[] mergeSort(int[] array) {
+        mov = 0;
+        comp = 0;
+        if (array.length > 1) {
             int elementsInA1 = array.length / 2;
             int elementsInA2 = array.length - elementsInA1;
             int arr1[] = new int[elementsInA1];
             int arr2[] = new int[elementsInA2];
-            for(int i = 0; i < elementsInA1; i++)
+            for (int i = 0; i < elementsInA1; i++)
                 arr1[i] = array[i];
-            for(int i = elementsInA1; i < elementsInA1 + elementsInA2; i++)
+            for (int i = elementsInA1; i < elementsInA1 + elementsInA2; i++)
                 arr2[i - elementsInA1] = array[i];
             arr1 = sortM(arr1);
             arr2 = sortM(arr2);
 
             int i = 0, j = 0, k = 0;
-            while(arr1.length != j && arr2.length != k)
-            {
-                if(arr1[j] < arr2[k])
-                {
+            while (arr1.length != j && arr2.length != k) {
+                if (arr1[j] < arr2[k]) {
                     array[i] = arr1[j];
                     i++;
                     j++;
-                }
-                else
-                {
+                    comp++;
+                    mov++;
+                } else {
                     array[i] = arr2[k];
                     i++;
                     k++;
+                    mov++;
                 }
             }
-            while(arr1.length != j)
-            {
+            while (arr1.length != j) {
                 array[i] = arr1[j];
                 i++;
                 j++;
+                mov++;
             }
-            while(arr2.length != k)
-            {
+            while (arr2.length != k) {
                 array[i] = arr2[k];
                 i++;
                 k++;
+                mov++;
             }
         }
         return array;
+    }
+    @Override
+    public int[] sortM(int[] array){
+        int []list = new int[array.length];
+        double ini = System.currentTimeMillis();
+        list = mergeSort(list);
+        double fin = System.currentTimeMillis();
+        super.report.addComparisons(comp);
+        super.report.addMovements(mov);
+        super.report.setTime(fin-ini);
+        return list;
     }
 }
